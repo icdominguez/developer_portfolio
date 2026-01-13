13544a8dcf7ccea3f0025c2defd3f722d409445c
icdominguez <56835908+icdominguez@users.noreply.github.com>
2026-01-13 16:19:16 +0100

    [004] Navigation
    - Navigation bar created with menu button
    - Theme set up
    - Button to change theme created

---
+++ src/features/experience/Experience.jsx
@@ -0,0 +1,10 @@
+export default function Experience() {
+    return (
+        <section
+            id="experience"
+            className="min-h-screen flex items-center justify-center"
+        >
+            <p>EXPERIENCE</p>
+        </section>
+    );
+}
---
+++ src/features/projects/Projects.jsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+    return (
+        <section
+            id="projects"
+            className="min-h-screen flex items-center justify-center bg-slate-50/50 dark:bg-slate-900/50"
+        >
+            <p>PROJECTS</p>
+        </section>
+    );
+}
