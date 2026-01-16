5d9578c4cc0445e43268debfc83ed30142e1106d
Ismael Cordón <56835908+icdominguez@users.noreply.github.com>
2026-01-16 13:25:51 +0100

    [008] Language and theme values persistence
    SettingsContext created in order to store locally the theme and language selected by the user but first taking in account the default system values.

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -1,22 +1,16 @@
 import { useEffect, useRef, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { LANGUAGES, useSettings } from "../../contexts/SettingsContext";
 
 export default function LanguageSelector() {
-    const { i18n } = useTranslation();
-
-    const languages = [
-        { code: "en", name: "English", flag: "https://flagcdn.com/gb.svg" },
-        { code: "es", name: "Español", flag: "https://flagcdn.com/es.svg" },
-    ];
+    const { language, switchLanguage } = useSettings();
 
     const [isOpen, setIsOpen] = useState(false);
-    const [selectedLang, setSelectedLang] = useState("en");
     const dropdownRef = useRef<HTMLDivElement | null>(null);
 
     const handleLanguageChange = (langCode: string) => {
-        setSelectedLang(langCode);
+        const selectedLanguage = LANGUAGES[langCode];
+        switchLanguage(selectedLanguage);
         setIsOpen(false);
-        i18n.changeLanguage(langCode);
     };
 
     useEffect(() => {
@@ -34,26 +28,24 @@ export default function LanguageSelector() {
             document.removeEventListener("mousedown", handleClickOutside);
     }, []);
 
-    const currentLanguage =
-        languages.find((lang) => lang.code === selectedLang) ?? languages[0];
     return (
         <div ref={dropdownRef} className="relative flex flex-col items-center">
             <button
                 onClick={() => setIsOpen(!isOpen)}
                 className="w-10 h-10 rounded-full bg-cover bg-center bg-no-repeat transition-transform duration-200 hover:scale-110"
                 style={{
-                    backgroundImage: `url(${currentLanguage.flag})`,
+                    backgroundImage: `url(${language.flag})`,
                 }}
             ></button>
 
             {/* Dropdown con círculos de banderas */}
             {isOpen && (
                 <div className="absolute top-14 left-1/2 -translate-x-1/2 flex flex-col gap-1.5 z-50 animate-in fade-in duration-200">
-                    {languages.map((lang) => (
+                    {Object.values(LANGUAGES).map((lang) => (
                         <button
                             key={lang.code}
                             className={`w-10 h-10 rounded-full bg-cover bg-center bg-no-repeat shadow-md transition-all duration-200 outline-none hover:scale-110 ${
-                                selectedLang === lang.code
+                                language.code === lang.code
                                     ? "border-3 border-blue-500"
                                     : "border-2 border-gray-300"
                             }`}
