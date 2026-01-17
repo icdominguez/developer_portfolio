656597b66d440baa87fb229cfd6129a5d44fb2c7
icdominguez <56835908+icdominguez@users.noreply.github.com>
2026-01-17 11:33:18 +0100

    [012] Home header
    - Hand shake animation.
    - Typing animation to show name and role.

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { SectionTitle } from "../../components/SectionTitle";
+import TypingText from "../../components/TypingText";
 
 export default function Home() {
     const { t } = useTranslation();
@@ -9,7 +9,20 @@ export default function Home() {
             id="home"
             className="min-h-screen flex items-center justify-center"
         >
-            <SectionTitle title={t("nav.home")} />
+            <div className="flex flex-col w-full items-center justify-center">
+                <div className="flex flow-col">
+                    <div className="flex flex-row">
+                        <span className="inline-block animate-wave text-3xl sm:text-4xl lg:text-6xl">
+                            👋
+                        </span>
+                        <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold px-4">
+                            {t("greeting")}
+                        </h1>
+                    </div>
+                </div>
+
+                <TypingText />
+            </div>
         </section>
     );
 }
