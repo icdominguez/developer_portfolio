b107d035ea2e4db0c3835c41b5173971eefdf586
Ismael Cordón <56835908+icdominguez@users.noreply.github.com>
2026-01-20 16:55:04 +0100

    [013] Terminal developer parallax 3D Effect

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,27 +1,35 @@
 import { useTranslation } from "react-i18next";
 import TypingText from "../../components/TypingText";
+import TerminalParallax3DEffect from "./TerminalParallax3DEffect";
 
 export default function Home() {
     const { t } = useTranslation();
 
     return (
-        <section
-            id="home"
-            className="min-h-screen flex items-center justify-center"
-        >
-            <div className="flex flex-col w-full items-center justify-center">
-                <div className="flex flow-col">
-                    <div className="flex flex-row">
-                        <span className="inline-block animate-wave text-3xl sm:text-4xl lg:text-6xl">
-                            👋
-                        </span>
-                        <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold px-4">
-                            {t("greeting")}
-                        </h1>
+        <section id="home">
+            <div className="w-full flex items-center justify-center pt-20 pb-20">
+                <div className="w-full max-w-7xl grid md:grid-cols-[1.1fr_0.9fr] items-start md:px-8">
+                    {/* Left Column */}
+                    <div className="w-full flex flex-col items-center md:items-start">
+                        <div className="flex flex-row items-center">
+                            <span className="inline-block animate-wave text-3xl sm:text-4xl lg:text-6xl">
+                                👋
+                            </span>
+                            <h1 className="text-3xl sm:text-4xl lg:text-6xl font-bold px-4">
+                                {t("greeting")}
+                            </h1>
+                        </div>
+
+                        <div className="mt-4">
+                            <TypingText />
+                        </div>
+                    </div>
+
+                    {/* Left Column */}
+                    <div className="w-full hidden md:block">
+                        <TerminalParallax3DEffect />
                     </div>
                 </div>
-
-                <TypingText />
             </div>
         </section>
     );
