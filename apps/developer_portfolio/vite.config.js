d791ffc356c5e75cdc6977ce3213428c9e100a3f
Ismael Cordón <56835908+icdominguez@users.noreply.github.com>
2026-05-25 17:13:15 +0200

    [034] Fixed workflow to run with node 24 and use the variables from secrets

--- apps/developer_portfolio/vite.config.js
+++ apps/developer_portfolio/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
     base: "/",
     plugins: [react(), tailwindcss(), svgr()],
